a3de4a370d1713a359587435287aeec8d1581028
Muhamad Ridho Budikusuma <77140390+Ridho894@users.noreply.github.com>
2022-01-16 21:48:00 +0700

    authentication (login, register, logout)

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,1 @@
+<h1>Welcome, Ridho</h1>
--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -9,16 +9,31 @@
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>
             @endif
+
+            @if (session()->has('loginError'))
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    <strong>{{ session('loginError') }}</strong>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
+
             <main class="form-signin">
                 <h1 class="h3 mb-3 fw-normal text-center">Please login</h1>
-                <form>
+                <form action="/login" method="POST">
+                    @csrf
                     <div class="form-floating">
-                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                        <label for="floatingInput">Email address</label>
+                        <input required autofocus name="email" type="email"
+                            class="form-control @error('email') is-invalid @enderror" id="email"
+                            placeholder="name@example.com" value="{{ old('email') }}">
+                        <label for="email">Email address</label>
+                        @error('email')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="form-floating">
-                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                        <label for="floatingPassword">Password</label>
+                        <input required name="password" type="password" class="form-control" id="password"
+                            placeholder="Password">
+                        <label for="password">Password</label>
                     </div>
                     <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
                 </form>
