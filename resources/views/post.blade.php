08a6b34a4743cdd302603f8ba5371e47089cfde7
Muhamad Ridho Budikusuma <77140390+Ridho894@users.noreply.github.com>
2022-10-10 21:08:57 +0700

    Update post.blade.php

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -12,8 +12,7 @@
                 </p>
                 @if ($post->image)
                     <div style="max-height: 350px; overflow: hidden;">
-                        <img src="{{ asset('storage/' . $post->image) }}" alt="{{ $post->category->name }}"
-                            class="img-fluid">
+                        <img src="{{ asset('storage/' . $post->image) }}" alt="{{ $post->category->name }}" class="img-fluid">
                     </div>
                 @else
                     <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}"
@@ -22,7 +21,7 @@
                 <article class="my-3 fs-5">
                     {!! $post->body !!}
                 </article>
-                <a href="/blog" class="d-block mt-3">Back to Posts</a>
+                <a href="/posts" class="d-block mt-3">Back to Posts</a>
             </div>
         </div>
     </div>
